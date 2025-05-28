991294bb9ab375d275cb817abe764f1ee0820423
Ashmit_kansal <ashmitkan@gmail.com>
2025-05-28 14:29:48 +0530

    completing the project

--- article-preview-component/README.md
+++ article-preview-component/README.md
@@ -1,12 +1,70 @@
-# React + Vite
+# Frontend Mentor - Article Preview Component Solution
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A responsive article preview card built with **React**, **Vite**, and **Tailwind CSS**.  
+This project is a solution to the [Frontend Mentor Article Preview Component Challenge](https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT).
 
-Currently, two official plugins are available:
+---
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Table of contents
 
-## Expanding the ESLint configuration
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+---
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- Share the article on Facebook, Twitter, and Pinterest
+- See hover and focus states for all interactive elements
+
+
+---
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Vite](https://vitejs.dev/) - Build tool
+
+### What I learned
+
+- How to toggle UI components using React state
+- Using Tailwind CSS for rapid and responsive styling
+- Creating a tooltip/overlay with a triangle using CSS borders and absolute positioning
+- Managing conditional rendering for mobile and desktop layouts
+- Accessibility considerations for interactive elements
+
+#### Example code
+
+```jsx
+<button
+  onClick={() => setShowShare(!ShowShare)}
+  className="ml-auto bg-grey-200 rounded-full p-3 z-10"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="15"
+    height="13"
+    className="text-grey-500"
+    viewBox="0 0 15 13"
+  >
+    <path
+      fill="currentColor"
+      d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+    />
+  </svg>
+</button>
--- article-preview-component/src/App.jsx
+++ article-preview-component/src/App.jsx
@@ -3,7 +3,6 @@ import drawers from "./assets/images/drawers.jpg";
 import facebookIcon from "./assets/images/icon-facebook.svg";
 import pinterestIcon from "./assets/images/icon-pinterest.svg";
 import twitterIcon from "./assets/images/icon-twitter.svg";
-import shareIcon from "./assets/images/icon-share.svg";
 
 import { useState } from "react";
 
@@ -31,32 +30,76 @@ function App() {
               it felt slightly bare and uninviting. I've got some simple tips to
               help you make any room feel complete.
             </p>
-            <div className={`flex items-center gap-4 mt-10 ${ShowShare ? "hidden md:flex":""}`}>
-              <img
-                className="w-[40px] rounded-full"
-                src={avatar}
-                alt="profile photo"
-              />
-              <div>
-                <h2 className="text-preset2-bold text-grey-900">
-                  Michelle Appleton
-                </h2>
-                <time
-                  className="text-preset2-medium text-grey-400"
-                  dateTime="2020-06-28"
+            <div className="relative">
+              {/* <-- This makes the share box position relative to this container */}
+              <div
+                className={`flex items-center gap-4 mt-10 ${
+                  ShowShare ? "hidden md:flex" : ""
+                }`}
+              >
+                <img
+                  className="w-[40px] rounded-full"
+                  src={avatar}
+                  alt="profile photo"
+                />
+                <div>
+                  <h2 className="text-preset2-bold text-grey-900">
+                    Michelle Appleton
+                  </h2>
+                  <time
+                    className="text-preset2-medium text-grey-400"
+                    dateTime="2020-06-28"
+                  >
+                    28 Jun 2020
+                  </time>
+                </div>
+                <button
+                  onClick={() => setShowShare(!ShowShare)}
+                  className="ml-auto bg-grey-200 rounded-full p-3 z-10"
                 >
-                  28 Jun 2020
-                </time>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="13"
+                    className="text-grey-500"
+                    viewBox="0 0 15 13"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+                    />
+                  </svg>
+                </button>
               </div>
+              {/* Desktop Share Box */}
+              {ShowShare && (
+                <div className="absolute bottom-full right-3 translate-x-1/2 mb-4 z-20">
+                  <div className="hidden md:flex items-center gap-4 bg-grey-900 p-6 rounded-xl">
+                    <p className="text-preset3 text-grey-400">SHARE</p>
+                    <img src={facebookIcon} alt="facebook icon" />
+                    <img src={twitterIcon} alt="twitter icon" />
+                    <img src={pinterestIcon} alt="pinterest icon" />
+                  </div>
+                  <div className="hidden md:block w-0 h-0 border-x-8 border-x-transparent border-t-8 border-t-grey-900 mx-auto"></div>
+                </div>
+              )}
+            </div>
+          </div>
+          {ShowShare && (
+            <div className="flex md:hidden items-center gap-4 bg-grey-900 p-6 rounded-b-lg">
+              <p className="text-preset3 text-grey-400">SHARE</p>
+              <img src={facebookIcon} alt="facebook icon" />
+              <img src={twitterIcon} alt="twitter icon" />
+              <img src={pinterestIcon} alt="pinterest icon" />
               <button
                 onClick={() => setShowShare(!ShowShare)}
-                className="ml-auto bg-grey-200 rounded-full p-3"
+                className="ml-auto bg-grey-500 rounded-full p-3"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="15"
                   height="13"
-                  className="text-grey-500"
+                  className="text-white"
                   viewBox="0 0 15 13"
                 >
                   <path
@@ -66,42 +109,7 @@ function App() {
                 </svg>
               </button>
             </div>
-          </div>
-          {ShowShare && (
-  <>
-    {/* Share component for md+ screens, positioned above the share icon */}
-    <div className="hidden md:flex items-center gap-4 bg-grey-900 p-6 rounded-xl absolute">
-      <p className="text-preset3 text-grey-400">SHARE</p>
-      <img src={facebookIcon} alt="facebook icon" />
-      <img src={twitterIcon} alt="twitter icon" />
-      <img src={pinterestIcon} alt="pinterest icon" />
-    </div>
-    {/* Share component for mobile screens (as before) */}
-    <div className="flex md:hidden items-center gap-4 bg-grey-900 p-6 rounded-b-lg">
-      <p className="text-preset3 text-grey-400">SHARE</p>
-      <img src={facebookIcon} alt="facebook icon" />
-      <img src={twitterIcon} alt="twitter icon" />
-      <img src={pinterestIcon} alt="pinterest icon" />
-      <button
-        onClick={() => setShowShare(!ShowShare)}
-        className="ml-auto bg-grey-500 rounded-full p-3"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="13"
-          className="text-white"
-          viewBox="0 0 15 13"
-        >
-          <path
-            fill="currentColor"
-            d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
-          />
-        </svg>
-      </button>
-    </div>
-  </>
-)}
+          )}
         </div>
       </div>
     </>
