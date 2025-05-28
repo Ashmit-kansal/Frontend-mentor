97a2f89c6587abe66186a827b3207a0d92fcab9d
Ashmit_kansal <ashmitkan@gmail.com>
2025-05-28 14:02:27 +0530

    Almost completed the project

--- article-preview-component/src/App.jsx
+++ article-preview-component/src/App.jsx
@@ -1,12 +1,111 @@
+import avatar from "./assets/images/avatar-michelle.jpg";
+import drawers from "./assets/images/drawers.jpg";
+import facebookIcon from "./assets/images/icon-facebook.svg";
+import pinterestIcon from "./assets/images/icon-pinterest.svg";
+import twitterIcon from "./assets/images/icon-twitter.svg";
+import shareIcon from "./assets/images/icon-share.svg";
 
+import { useState } from "react";
 
 function App() {
+  const [ShowShare, setShowShare] = useState(false);
 
   return (
     <>
-      
+      <div className="min-h-screen bg-grey-200 flex items-center justify-center">
+        <div className="w-[80%] max-w-[700px] bg-white rounded-lg shadow-lg md:flex">
+          <div className="max-h-[220px] md:max-h-none overflow-hidden">
+            <img
+              className="rounded-t-lg md:rounded-l-lg md:rounded-t-none md:h-[100%]"
+              src={drawers}
+              alt="drawers image"
+            />
+          </div>
+          <div className="p-6">
+            <h1 className="text-preset1 text-grey-900 my-2">
+              Shift the overall look and feel by adding these wonderful touches
+              to furniture in your home
+            </h1>
+            <p className="text-preset2-medium text-grey-500 my-6">
+              Ever been in a room and felt like something was missing? Perhaps
+              it felt slightly bare and uninviting. I've got some simple tips to
+              help you make any room feel complete.
+            </p>
+            <div className={`flex items-center gap-4 mt-10 ${ShowShare ? "hidden md:flex":""}`}>
+              <img
+                className="w-[40px] rounded-full"
+                src={avatar}
+                alt="profile photo"
+              />
+              <div>
+                <h2 className="text-preset2-bold text-grey-900">
+                  Michelle Appleton
+                </h2>
+                <time
+                  className="text-preset2-medium text-grey-400"
+                  dateTime="2020-06-28"
+                >
+                  28 Jun 2020
+                </time>
+              </div>
+              <button
+                onClick={() => setShowShare(!ShowShare)}
+                className="ml-auto bg-grey-200 rounded-full p-3"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="13"
+                  className="text-grey-500"
+                  viewBox="0 0 15 13"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          {ShowShare && (
+  <>
+    {/* Share component for md+ screens, positioned above the share icon */}
+    <div className="hidden md:flex items-center gap-4 bg-grey-900 p-6 rounded-xl absolute">
+      <p className="text-preset3 text-grey-400">SHARE</p>
+      <img src={facebookIcon} alt="facebook icon" />
+      <img src={twitterIcon} alt="twitter icon" />
+      <img src={pinterestIcon} alt="pinterest icon" />
+    </div>
+    {/* Share component for mobile screens (as before) */}
+    <div className="flex md:hidden items-center gap-4 bg-grey-900 p-6 rounded-b-lg">
+      <p className="text-preset3 text-grey-400">SHARE</p>
+      <img src={facebookIcon} alt="facebook icon" />
+      <img src={twitterIcon} alt="twitter icon" />
+      <img src={pinterestIcon} alt="pinterest icon" />
+      <button
+        onClick={() => setShowShare(!ShowShare)}
+        className="ml-auto bg-grey-500 rounded-full p-3"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="15"
+          height="13"
+          className="text-white"
+          viewBox="0 0 15 13"
+        >
+          <path
+            fill="currentColor"
+            d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+          />
+        </svg>
+      </button>
+    </div>
+  </>
+)}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
